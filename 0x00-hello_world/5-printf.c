512f2f5c78cf6c4146e4f08282ec873763cffb85
nahfeesah <inofisat5@gmail.com>
2023-04-23 15:02:31 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/* main-entry */
+/* Description- using printf*/
+/* Return:Always 0 */
+int main(void)
+{
+	printf("with proper grammar,but the outcome is a piece of art,");
+	return (0);
+}
